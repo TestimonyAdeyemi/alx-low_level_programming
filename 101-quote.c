22c34d17ea869647887070ede09423763b254517
TestimonyAdeyemi <adeyemitestimony@gmail.com>
2021-02-11 20:23:08 +0000

    C

---
+++ 101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*quote - prints a quote
+*
+*Return:always 0 (success)
+*/
+int main()
+{
+char doublequotes = '"';
+char space = ' ';
+char dash = '-';
+char comma = ','
+printf("and that piece of art is useful%s%s%sDora Korpar%s%s2015%s10%s19\n", doublequotes, space, dash, comma, space, dash, dash);
+return(0)
+}
